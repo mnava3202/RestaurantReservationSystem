ee5f648c8933b828ed9190acbcc4cb89b5115dbd
Michael Nava <michael.nava@marquette.edu>
2023-12-05 14:43:17 -0600

    Created a Reservation class. Added a date method.

---
+++ RestaurantReservationSystem/Model/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservationSystem.Model
+{
+    internal class Reservation
+    {
+        public DateTime date { get; set; }
+
+    }
+}
