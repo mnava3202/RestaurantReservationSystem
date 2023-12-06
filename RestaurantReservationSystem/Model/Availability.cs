14e756b87cc7e13f7dcb958251bae772e1a354dd
daniel-nava23 <daniel.nava@marquette.edu>
2023-12-06 11:55:27 -0600

    Added methods to Availability class.

--- RestaurantReservationSystem/Model/Availability.cs
+++ RestaurantReservationSystem/Model/Availability.cs
@@ -3,8 +3,35 @@ namespace RestaurantReservationSystem.Model
 {
 	public class Availability
 	{
+		public List<Table> freeTables;
+
+		public List<Table> reservedTables; 
+
 		public Availability()
 		{
+			freeTables = new List<Table>();
+
+			reservedTables = new List<Table>();
+		}
+
+		public void addFreeTable(Table t)
+		{
+			freeTables.Add(t);
+		}
+
+		public List<Table> getFreeTables()
+		{
+			return freeTables;
+		}
+
+		public void addReservedTables(Table t)
+		{
+			reservedTables.Add(t);
+		}
+
+		public List<Table> getReservedTables()
+		{
+			return reservedTables;
 		}
 	}
 }
--- RestaurantReservationSystem/Model/Table.cs
+++ RestaurantReservationSystem/Model/Table.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RestaurantReservationSystem.Model
 {
-    internal class Table
+    public class Table
     {
         public int ID { get; set; }
 
