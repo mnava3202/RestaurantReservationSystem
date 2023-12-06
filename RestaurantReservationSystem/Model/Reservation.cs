5e2f92bf2056d41f0e8065235341498773b36f46
Michael Nava <michael.nava@marquette.edu>
2023-12-06 11:53:28 -0600

    Updated getters and setters for all methods in Reservation class.

--- RestaurantReservationSystem/Model/Reservation.cs
+++ RestaurantReservationSystem/Model/Reservation.cs
@@ -8,12 +8,79 @@ namespace RestaurantReservationSystem.Model
 {
     internal class Reservation
     {
-        public DateTime date { get; set; }
-        public int NumOfGuests { get; set; }
+        private DateTime date;
 
-        public string Occasion { get; set; }
+        public DateTime GetDate() 
+        {
+            return date;
+        }
 
-        public string RoomType { get; set; }
+        public void SetDate( string t) 
+        {
+            if (t != "Sunday" && t != "Monday" && t != "Tuesday" && t != "Wednesday" && t != "Thursday" && t != "Friday" && t != "Saturday")
+            {
+                Console.WriteLine("Invalid day.");
+            }
+            else 
+            {
+                date = DateTime.Parse(t);
+            }
+        }
+
+        /////////////////////////////////////////////////
+        private int NumOfGuests;
+
+        public int GetNumOfGuests() 
+        {
+            return NumOfGuests;
+        }
+
+        public void SetNumOfGuests(int g) 
+        {
+            if (g > 0) 
+            {
+                NumOfGuests = g;
+            }
+        }
+        /////////////////////////////////////////////////
+        private string Occasion;
+
+        public string GetOccasion() 
+        {
+            return Occasion;
+        }
+
+        public void SetOccasion(string o) 
+        {
+            if (o != "Business" && o != "Birthday" && o != "Anniversary" && o != "Regular")
+            {
+                Console.WriteLine("Invalid occassion. Please type 'Business', 'Birthday', 'Anniversaty' or 'Regular'.");
+            }
+            else
+            { 
+                Occasion = o; 
+            }
+        }
+        /////////////////////////////////////////////////
+        private string RoomType;
+
+        public string GetRoomType() 
+        {
+            return RoomType;
+        }
+
+        public void SetRoomType (string r) 
+        {
+            if (r != "Indoor" && r != "Outdoor")
+            {
+                Console.WriteLine("Invalid room type. Please type 'Indoor' or 'Outdoor'.");
+            }
+            else 
+            {
+                RoomType = r;
+            }
+           
+        }
 
 
     }
