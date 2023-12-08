406d920fee61b7166bb2e9f9878f0fc4a3cf5ef8
patrickbreids <patrick.breidsprecher@marquette.edu>
2023-12-08 13:48:03 -0600

    comments

--- RestaurantReservationSystem/Model/Customer.cs
+++ RestaurantReservationSystem/Model/Customer.cs
@@ -8,11 +8,11 @@ namespace RestaurantReservationSystem.Model;
 
 public class Customer
 {
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } // creates attribute of frst name, so that customers can input their name when making a resrvation
 
-    public string LastName { get; set; }
+    public string LastName { get; set; } // creates attribute of last name, so that customers can input their last name when making a resrvation
 
-    public string Email { get; set; }
+    public string Email { get; set; } // creates attribute of email, so that customers can input their email when making a resrvation
 
-    public string PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; } // creates attribute of phone number, so that customers can input their phone number when making a resrvation
 }
